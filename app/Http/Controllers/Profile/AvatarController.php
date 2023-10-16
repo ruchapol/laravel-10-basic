0a659aeadf1ee7f0a9eae93e905fb65415562d32
Ruchapol <ruchapol.sripruetkiat@iamconsulting.co.th>
2023-10-16 22:57:44 +0700

    move request form validation logic to another part

--- app/Http/Controllers/Profile/AvatarController.php
+++ app/Http/Controllers/Profile/AvatarController.php
@@ -3,15 +3,12 @@
 namespace App\Http\Controllers\Profile;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\UpdateAvatarRequest;
 use Illuminate\Http\Request;
 
 class AvatarController extends Controller
 {
-    public function update(Request $request) {
-        
-        $request->validate([
-            'avatar' => ['required', 'image'],
-        ]);
+    public function update(UpdateAvatarRequest $request) {
 
         dd($request->all());
 
