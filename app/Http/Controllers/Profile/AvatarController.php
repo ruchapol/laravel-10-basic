128644c7c7d18614b421101d33b570108bacd81e
Ruchapol <ruchapol.sripruetkiat@iamconsulting.co.th>
2023-10-17 01:21:34 +0700

    use facades storage instead of store function

--- app/Http/Controllers/Profile/AvatarController.php
+++ app/Http/Controllers/Profile/AvatarController.php
@@ -4,17 +4,21 @@ namespace App\Http\Controllers\Profile;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\UpdateAvatarRequest;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class AvatarController extends Controller
 {
     public function update(UpdateAvatarRequest $request) {
-
-        // auth()->user()->update(['avatar' => 'reddd']);
         
-        $path = $request->file('avatar')->store('avatars', 'public');
+        $path = Storage::disk('public')->put('avatars', $request->file('avatar'));
+        // $path = $request->file('avatar')->store('avatars', 'public');
+        
+        if($oldAvatar = $request->user()->avatar) {
+            Storage::disk('public')->delete($oldAvatar);
+        }
+        
         auth()->user()->update(['avatar' => $path]);
-
+        
         return redirect(route('profile.edit'))->with('message', 'Avatar is updated.');
     }
 }
