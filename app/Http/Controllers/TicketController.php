74cd9a61040bf2d1f84ff9b4debc0295c0bf7289
Ruchapol <ruchapol.sripruetkiat@iamconsulting.co.th>
2023-10-19 00:03:42 +0700

    add delete and edit button then add destroy logic

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -87,6 +87,7 @@ class TicketController extends Controller
     */
     public function destroy(Ticket $ticket)
     {
-        //
+        $ticket->delete();
+        return redirect(route('ticket.index'));
     }
 }
