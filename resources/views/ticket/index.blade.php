5732129237dbfd4ad854a74fbe24109762bc7887
Ruchapol <ruchapol.sripruetkiat@iamconsulting.co.th>
2023-10-20 01:08:08 +0700

    add text to index ticket when there is no any ticket

--- resources/views/ticket/index.blade.php
+++ resources/views/ticket/index.blade.php
@@ -10,12 +10,15 @@
         </div>
         
         <div class="w-full sm:max-w-xl mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
-            @foreach ($tickets as $ticket)
-                <div class="text-white flex justify-between py-4">
-                    <a href="{{ route('ticket.show', $ticket->id) }}">{{ $ticket->title }}</a>
-                    <p>{{ $ticket->created_at->diffForHumans() }}</p>
-                </div>
-            @endforeach
+            @forelse ($tickets as $ticket)
+            <div class="text-white flex justify-between py-4">
+                <a href="{{ route('ticket.show', $ticket->id) }}">{{ $ticket->title }}</a>
+                <p>{{ $ticket->created_at->diffForHumans() }}</p>
+            </div>
+            @empty
+            <p class="text-white">You don't have any support ticket yet.</p>
+            @endforelse
+            
         </div>
     </div>
 </x-app-layout>
