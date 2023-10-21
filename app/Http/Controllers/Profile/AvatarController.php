639751fdf8b906bd38c0d97fd140ba89c1b2c388
Ruchapol <ruchapol.sripruetkiat@iamconsulting.co.th>
2023-10-21 15:10:16 +0700

    add logic to delete old image after the user update new avatar

--- app/Http/Controllers/Profile/AvatarController.php
+++ app/Http/Controllers/Profile/AvatarController.php
@@ -5,12 +5,20 @@ namespace App\Http\Controllers\Profile;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\UpdateAvatarRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class AvatarController extends Controller
 {
     public function update(UpdateAvatarRequest $request) {
 
-    
+        $path = $request->file('avatar')->store('avatars', 'public');
+
+        if ($oldAvatar = $request->user()->avatar) {
+            Storage::disk('public')->delete($oldAvatar);
+        }
+
+        auth()->user()->update(['avatar' => $path ]);
+
 
         return response()->redirectTo(route('profile.edit'))->with('message', 'Avatar is changed.');
     }
