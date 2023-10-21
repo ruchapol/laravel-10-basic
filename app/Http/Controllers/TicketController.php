75dc3b3c0111dfaae5328b2a13cb7f0c113f72d1
Ruchapol <ruchapol.sripruetkiat@iamconsulting.co.th>
2023-10-21 18:39:27 +0700

    add create ticket method on controller

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -5,60 +5,80 @@ namespace App\Http\Controllers;
 use App\Models\Ticket;
 use App\Http\Requests\StoreTicketRequest;
 use App\Http\Requests\UpdateTicketRequest;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 class TicketController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     */
+    * Display a listing of the resource.
+    */
     public function index()
     {
         //
     }
-
+    
     /**
-     * Show the form for creating a new resource.
-     */
+    * Show the form for creating a new resource.
+    */
     public function create()
     {
-        return view("ticket.create");
+        
     }
-
+    
     /**
-     * Store a newly created resource in storage.
-     */
+    * Store a newly created resource in storage.
+    */
     public function store(StoreTicketRequest $request)
     {
-        //
+        $ticket = Ticket::create([
+            'title' => $request->title,
+            'description' => $request->description,
+            'user_id' => auth()->id(),
+        ]);
+        
+        if ($request->file('attachment')) {
+            
+            $ext = $request->file('attachment')->extension();  // 12313.txt extention => ".txt"
+            $contents = file_get_contents($request->file('attachment'));
+            
+            $filename = Str::random(25);
+            $path = "attachments/$filename.$ext";
+            Storage::disk('public')->put($path, $contents);
+            $ticket->update(['attachment'=>$path]);
+        }
+        
+        return response()->redirect(route('ticket.index'));
     }
-
+    
     /**
-     * Display the specified resource.
-     */
+    * Display the specified resource.
+    */
     public function show(Ticket $ticket)
     {
         //
     }
-
+    
     /**
-     * Show the form for editing the specified resource.
-     */
+    * Show the form for editing the specified resource.
+    */
     public function edit(Ticket $ticket)
     {
         //
     }
-
+    
     /**
-     * Update the specified resource in storage.
-     */
+    * Update the specified resource in storage.
+    */
     public function update(UpdateTicketRequest $request, Ticket $ticket)
     {
-        //
+        dd($request->all());
+        return "";
     }
-
+    
     /**
-     * Remove the specified resource from storage.
-     */
+    * Remove the specified resource from storage.
+    */
     public function destroy(Ticket $ticket)
     {
         //
