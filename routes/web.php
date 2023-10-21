ec0162f29ae4a2c8c454a5b651bb968de8eefd4f
Ruchapol <ruchapol.sripruetkiat@iamconsulting.co.th>
2023-10-21 15:49:43 +0700

    add ticket create button at the navigation bar, use ticketController, add create ticket view, add ticket route(api)

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Profile\AvatarController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TicketController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -28,6 +29,14 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::patch('/profile/avatar', [AvatarController::class, 'update'])->name('profile.avatar');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+
+Route::middleware('auth')->group( function() {
+    
+    Route::middleware('auth')->group( function() {
+        Route::resource('ticket', TicketController::class);  // will add crud api routes for ticket 
+    });
 });
 
 require __DIR__.'/auth.php';
