09beae3db9a97270ba0cfba107b897c437d8e462
Ruchapol <ruchapol.sripruetkiat@iamconsulting.co.th>
2023-10-21 15:34:11 +0700

    update ticket migration file and enum

--- database/migrations/2023_10_21_081637_create_tickets_table.php
+++ database/migrations/2023_10_21_081637_create_tickets_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Enums\TicketStatus;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,8 +14,21 @@ return new class extends Migration
     {
         Schema::create('tickets', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->string('status')->default(TicketStatus::OPEN->value);
+            $table->string('attachment')->nullable();
+            $table->foreignId('user_id')->constrained();
+            // incase of table does not match the Laravel's conventions
+            //
+            //$table->foreignId('user_id')->constrained(
+            //   table: 'users', indexName: 'posts_user_id'
+            //); 
+            
+            $table->foreignId('status_changed_by_id')->nullable()->constrained('users');
             $table->timestamps();
         });
+
     }
 
     /**
