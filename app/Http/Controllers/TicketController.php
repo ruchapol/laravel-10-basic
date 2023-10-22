48855ac520ff102dd705cac9993508c14d374172
Ruchapol <ruchapol.sripruetkiat@iamconsulting.co.th>
2023-10-22 15:39:40 +0700

    add user notify email when the card status is resolved

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -6,6 +6,7 @@ use App\Models\Ticket;
 use App\Http\Requests\StoreTicketRequest;
 use App\Http\Requests\UpdateTicketRequest;
 use App\Models\User;
+use App\Notifications\TicketUpdatedNotification;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
@@ -16,10 +17,10 @@ class TicketController extends Controller
     */
     public function index()
     {
-
+        
         $user = auth()->user();
         $tickets = $user->isAdmin ? $tickets = Ticket::all() : $tickets = $user->tickets;
-
+        
         return view('ticket.index', compact('tickets'));
     }
     
@@ -75,12 +76,17 @@ class TicketController extends Controller
     */
     public function update(UpdateTicketRequest $request, Ticket $ticket)
     {
-
+        
         // $ticket->update(['title'=> $request->title, 
         // 'description'=> $request->description]);
-
+        
         $ticket->update($request->except('attachment'));
-
+        
+        if ($request->has('status')) {
+            $user = User::find($ticket->user_id);
+            $user->notify(new TicketUpdatedNotification($ticket));
+        }
+        
         
         if ($request->file('attachment')) {
             Storage::disk('public')->delete($ticket->attachment);
